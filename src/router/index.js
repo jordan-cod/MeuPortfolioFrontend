1305f63b4dff56f590080673c13127a2a8f01a02
Gabriel Jordan <gabrijordan3@gmail.com>
2023-12-04 20:01:44 -0300

    remove default archives

--- src/router/index.js
+++ src/router/index.js
@@ -7,15 +7,6 @@ const routes = [
     name: 'home',
     meta: {title: ''},
     component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    meta: {title: ''},
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   }
 ]
 

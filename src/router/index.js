a2f96805ab8df94115e342f45b2f48c4a3958ea8
Gabriel Jordan <gabrijordan3@gmail.com>
2023-12-04 19:56:27 -0300

    Add meta title per page

--- src/router/index.js
+++ src/router/index.js
@@ -5,21 +5,28 @@ const routes = [
   {
     path: '/',
     name: 'home',
+    meta: {title: ''},
     component: HomeView
   },
   {
     path: '/about',
     name: 'about',
+    meta: {title: ''},
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue'),
   }
 ]
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  document.title = `Gabriel Jordan  ${to.meta.title}`
+  next()
 })
 
 export default router

845ba77b07691bd2ab06d3169aacc9ff575eee7e
Gabriel Jordan <gabrijordan3@gmail.com>
2023-12-04 21:01:12 -0300

    create Header

--- src/components/HeaderComponent.vue
+++ src/components/HeaderComponent.vue
@@ -4,14 +4,15 @@
             <nav>
                 <ul>
                     <li>
-                        <rounter-link to="/">Home</rounter-link>
+                        <RouterLink to="/">Home</RouterLink>
                     </li>
                     <li>
-                        <rounter-link to="/projects">Projects</rounter-link>
+                        <RouterLink to="/projects">Projects</RouterLink>
                     </li>
                 </ul>
             </nav>
         </div>
+        <img src="../assets/HeaderSVG.svg">
     </header>
 </template>
 
@@ -20,5 +21,36 @@
         width: 100%;
         height: 60px;
         background: #23272A;
+        color: white;
+    }
+    header div{
+        width: 80%;
+    }
+    header div, header div nav{
+        height: 100%;
+        display: flex;
+        align-items: center;
+        flex-direction: row-reverse;
+    }
+    header ul {
+        height: 100%;
+        display: flex;
+        align-items: center;
+    }
+    header nav a {
+        color: white;
+        text-transform: uppercase;
+        font-weight: 500;
+        margin-left: 30px;
+        padding: 16px 0px;
+    }
+    header nav a:hover{
+        color: #0015FF;
+    }
+    header nav a.router-link-exact-active {
+    color: #0015FF;
+    }
+    header nav a.router-link-exact-active {
+    border-bottom: 3px solid #0015FF;
     }
 </style>

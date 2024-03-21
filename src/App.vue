3d1b447350e3bee6cd4a8fcacb3efcf13826d543
Gabriel Jordan <gabrijordan3@gmail.com>
2024-03-21 12:15:41 -0300

    add scroll up button

--- src/App.vue
+++ src/App.vue
@@ -5,6 +5,9 @@
       <Transition name="page-slide" mode="out-in">
         <component :is="Component"/>
       </Transition>
+      <div v-show="this.scroll" class="scrollUp" @click="scrollUp">
+        <i class="fa-solid fa-arrow-up"></i>
+      </div>
     </router-view>
   </div>
 </template>
@@ -14,6 +17,31 @@ import '@/css/global.css'
 import HeaderComponent from './components/HeaderComponent.vue';
 export default {
   name: 'App',
+  data (){
+    return {
+      scroll : false
+    }
+  },
+  created () {
+    window.addEventListener('scroll', this.handleScroll);
+    this.handleScroll
+  },
+  unmounted () {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    handleScroll () {
+      if (window.scrollY > 250){
+        this.scroll = true
+      }
+      else {
+        this.scroll = false
+      }
+    },
+    scrollUp(){
+      window.scrollTo(0, top);
+    }
+  },
   components: {HeaderComponent},
   beforeMount() {
     this.$store.commit('GetProjects')
@@ -39,5 +67,23 @@ export default {
   .page-slide-enter-from,
   .page-slide-leave-to{
     transform: translateY(60px)
+  }
+  .scrollUp {
+    width: 55px;
+    height: 55px;
+    border-radius: 50%;
+    background:var(--blue);
+    z-index: 10;
+    position: fixed;
+    bottom: 20px;
+    right: 20px;
+    cursor: pointer;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .scrollUp i{
+    font-size: 25px;
+    color: var(--white);
   }
 </style>

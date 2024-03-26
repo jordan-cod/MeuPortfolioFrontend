8e47b7d1976188b0c52bc8d0c3d4cd64063369af
Gabriel Jordan <gabrijordan3@gmail.com>
2024-03-26 17:00:02 -0300

    change cv file

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,7 +13,7 @@
           <a href="https://www.linkedin.com/in/gabrieljordandev/" target="_blank">
             <i class="fa-brands fa-linkedin-in fa-2xl" style="color: #337eff;"></i>
           </a>
-          <a href="/files/cv.pdf" class="CVDownload" download="">
+          <a href="/files/Gabrie_Jordan_resume.pdf" class="CVDownload">
             <i class="fa-solid fa-file-arrow-down fa-xl"></i>CV
           </a>
         </div>
